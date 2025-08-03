641887c54cb25a1c95962b5b500d76a7b4151a13
lnoxsian <lnox122224@proton.me>
2025-08-03 17:21:42 +0530

    finalize the container sript

--- scripts/dockbuild.sh
+++ scripts/dockbuild.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/bash
 
 # just a build script for running the build repo in docker container
-
 docker_exec_bash() {
     local CONTAINER_NAME="$1"
     shift
     local COMMAND="$@"
     docker exec -it "$CONTAINER_NAME" bash -c "$COMMAND"
+}
+
+# Function to stop the docker container
+docker_stop() {
+    local CONTAINER_NAME="$1"
+    docker stop "$CONTAINER_NAME"
 }
 
 # Function to copy file from container to host
@@ -17,25 +22,28 @@ docker_copy() {
     docker cp "${CONTAINER_NAME}:${SRC_PATH}" "${DEST_PATH}"
 }
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <container_name> <docker_image>"
-    exit 1
-fi
+# Function for creating the docker container
+docker_run() {
+    local CONTAINER_NAME="$1"
+    local DOCKER_IMAGE="$2"
+    docker run -dt \
+        --rm \
+        --privileged \
+        --cap-add=SYS_CHROOT \
+        --name "$CONTAINER_NAME" \
+        --hostname "${CONTAINER_NAME}local" \
+        "$DOCKER_IMAGE"
+}
 
+
+# Default values for the env vars
 CONTAINER_NAME="dockdeb"
 DOCKER_IMAGE="debian"
 GITHUB_REPO="https://github.com/lnoxsian/debian-live-config.git"
 GITHUB_REPO_DIR="debian-live-config-test"
 GEN_ISO="live-image-amd64.hybrid.iso"
 
-docker run -dt \
-    --runtime=nvidia \
-    --gpus all \
-    --privileged \
-    --cap-add=SYS_CHROOT \
-    --name "$CONTAINER_NAME" \
-    --hostname "${CONTAINER_NAME}local" \
-    "$DOCKER_IMAGE"
+docker_run "$CONTAINER_NAME" "$DOCKER_IMAGE"
 
 docker_exec_bash "$CONTAINER_NAME" "apt update -y && apt upgrade -y && apt install make git sudo live-build -y"
 
@@ -43,4 +51,6 @@ docker_exec_bash "$CONTAINER_NAME" "git clone $GITHUB_REPO $GITHUB_REPO_DIR"
 
 docker_exec_bash "$CONTAINER_NAME" "cd $GITHUB_REPO_DIR && make install_buildenv && make"
 
-docker_copy "$CONTAINER_NAME" "/root/$GITHUB_REPO_DIR/$GEN_ISO" .
+docker_copy "$CONTAINER_NAME" "/$GITHUB_REPO_DIR/$GEN_ISO" .
+
+docker_stop "$CONTAINER_NAME" 
